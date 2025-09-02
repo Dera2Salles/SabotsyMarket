17f463f4c7eacd19d4369eb27c8546ed512957db
Dera2S <dera.ah.14@gmail.com>
2025-09-02 19:40:48 +0300

    fix : fixing entry point code

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { ProductProvider } from "./presentation/context/productProvider";
 import { NotificationWithTimer } from "./components/ui/notification";
 import { PageRoutes } from "./presentation/routes";
+import { AuthProvider } from "./presentation/pages/Login/context/useAuthProvider";
 
 const App = () => {
   return (
-    <div className=" bg-zinc-100 min-h-auto">
+    <div className=" bg-zinc-100 h-full">
       <ProductProvider>
-        <PageRoutes />
+        <AuthProvider>
+          <PageRoutes />
+        </AuthProvider>
       </ProductProvider>
       <NotificationWithTimer />
     </div>
