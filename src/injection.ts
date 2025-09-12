cc153cf11edeca37e6280395f260979c9b5ce1ea
Dera2S <dera.ah.14@gmail.com>
2025-09-12 12:16:07 +0300

    style : formatting code

--- src/injection.ts
+++ src/injection.ts
@@ -1,22 +1,22 @@
-import { ProductRepositoryImp } from "./product/application/ProductRepository";
+import { ProductRepositoryImp } from './product/application/ProductRepository';
 
-import { GetAllProduct } from "./product/application/Product/GetAllProduct";
-import { GetOneProduct } from "./product/application/Product/GetOneProduct";
-import { InsertManyProduct } from "./product/application/Product/InsertManyProduct";
-import { ProductRemoteDataSource } from "./product/application/data/product_remote_data_source";
-import axios from "axios";
-import { AddProductToTheOrder } from "./order/application/Order/addProductOrder";
-import { OrderRepositoryImpl } from "./order/application/repository/OrderRepositoryImpl";
-import { CreateAnOrder } from "./order/application/Order/createAnOrder";
-import { RemoveProductOrder } from "./order/application/Order/removeProduct";
-import { AuthServiceImpl } from "./auth/application/data/auth_service";
-import { LoginUseCase } from "./auth/application/use_case/login";
-import { SendFile } from "./product/application/Product/senFiles";
-import { OrderRemoteDataSourceImpl } from "./order/application/data/order_server";
-import { ConfirmOrderUseCase } from "./order/application/Order/confirm_order";
-import { GetUserDataUseCase } from "./auth/application/use_case/get";
-import { DeleteProductUseCase } from "./product/application/Product/delete";
-import { AuthRepositoryImpl } from "./auth/application/repository/AuthRepository";
+import { GetAllProduct } from './product/application/Product/GetAllProduct';
+import { GetOneProduct } from './product/application/Product/GetOneProduct';
+import { InsertManyProduct } from './product/application/Product/InsertManyProduct';
+import { ProductRemoteDataSource } from './product/application/data/product_remote_data_source';
+import axios from 'axios';
+import { AddProductToTheOrder } from './order/application/Order/addProductOrder';
+import { OrderRepositoryImpl } from './order/application/repository/OrderRepositoryImpl';
+import { CreateAnOrder } from './order/application/Order/createAnOrder';
+import { RemoveProductOrder } from './order/application/Order/removeProduct';
+import { AuthServiceImpl } from './auth/application/data/auth_service';
+import { LoginUseCase } from './auth/application/use_case/login';
+import { SendFile } from './product/application/Product/senFiles';
+import { OrderRemoteDataSourceImpl } from './order/application/data/order_server';
+import { ConfirmOrderUseCase } from './order/application/Order/confirm_order';
+import { GetUserDataUseCase } from './auth/application/use_case/get';
+import { DeleteProductUseCase } from './product/application/Product/delete';
+import { AuthRepositoryImpl } from './auth/application/repository/AuthRepository';
 
 const api = axios.create({
   timeout: 5000,
@@ -43,17 +43,17 @@ const orderRepository = new OrderRepositoryImpl(orderServiceServer);
 export const getAllProductUseCase = new GetAllProduct(productRepository);
 export const getOneProductUseCase = new GetOneProduct(productRepository);
 export const insertManyProductUseCase = new InsertManyProduct(
-  productRepository
+  productRepository,
 );
 export const confirmOrderUseCase = new ConfirmOrderUseCase(orderRepository);
 
 export const sendFilesUseCase = new SendFile(productRepository);
 export const addProductToTheOrderUseCase = new AddProductToTheOrder(
-  orderRepository
+  orderRepository,
 );
 export const createAnOrderUseCase = new CreateAnOrder(orderRepository);
 export const removeProductOrderUseCase = new RemoveProductOrder(
-  orderRepository
+  orderRepository,
 );
 
 export const deleteProductUseCase = new DeleteProductUseCase(productRepository);
